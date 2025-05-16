c55dd15c4843d9789b944a29675d8785d9e59aaf
Fernando <toshk15@hotmail.com>
2025-05-16 13:50:28 -0600

    selection shellsort

--- src/app.py
+++ src/app.py
@@ -8,10 +8,26 @@ def selectionSort(arr):
     return arr
 
 
+def shellSort(arr):
+    interval = len(arr) // 2
+    while interval > 0:
+        for i in range(interval, len(arr)):
+            temp = array[i]
+            j = if
+            while j >= 0 and array[j - interval] > temp:
+                array[j] = array[j - interval]
+                j -= interval
+            
+            array[j] = temp
+        interval //= 2
+    return array
+    
+
 
 if __name__ == "__main__":
 
     arr = [10, 30, 3, 6, 1, 23]
     
     print(selectionSort(arr))
+    print(shellSort(arr))
 
--- test/test_app.py
+++ test/test_app.py
@@ -1,6 +1,10 @@
-from src.app import selectionSort
+from src.app import selectionSort, shellSort
 
 def test_selectionSort_function():
     assert selectionSort([10, 30, 3, 6, 1, 23]) == [1, 3, 6, 10, 23, 30]
+
+def test_shellSort_function():
+    assert shellSort([10, 30, 3, 6, 1, 23]) == [1, 3, 6, 10, 23, 30]
+ 
  
 
