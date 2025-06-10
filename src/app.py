0e9687d9a7833335ad36bcf3cc31513ca3f9dad4
Fernando <toshk15@hotmail.com>
2025-06-10 09:18:09 -0600

    count sort

--- src/app.py
+++ src/app.py
@@ -21,6 +21,29 @@ def shellSort(arr):
             arr[j] = temp
         interval //= 2
     return arr
+
+
+def countSort(arr):
+    
+    cr = []    
+    n = len(arr)
+    maxval = max(arr)
+    cr = [0] * (maxval + 1)
+
+    for i in range(0, n):
+        cr[arr[i]] += 1
+
+    i, j = 0, 0
+    while j < maxval + 1:
+        if cr[j] > 0:
+            arr[i] = j
+            i+=1
+            cr[j]-=1
+        else:
+            j+=1
+    return arr
+
+
     
 
 
--- test/test_app.py
+++ test/test_app.py
@@ -1,4 +1,4 @@
-from src.app import selectionSort, shellSort
+from src.app import selectionSort, shellSort, countSort
 
 def test_selectionSort_function():
     assert selectionSort([10, 30, 3, 6, 1, 23]) == [1, 3, 6, 10, 23, 30]
@@ -7,8 +7,13 @@ def test_selectionSort_function():
 
 def test_shellSort_function():
     assert shellSort([10, 30, 3, 6, 1, 23]) == [1, 3, 6, 10, 23, 30]
-    assert selectionSort([1, 18, 3, 9, 1, 3]) == [1, 1, 3, 3, 9, 18]
-    assert selectionSort([22, 3, 12, 6, 2, 20]) == [2, 3, 6, 12, 20, 22]
+    assert shellSort([1, 18, 3, 9, 1, 3]) == [1, 1, 3, 3, 9, 18]
+    assert shellSort([22, 3, 12, 6, 2, 20]) == [2, 3, 6, 12, 20, 22]
+
+def test_countSort_function():
+    assert countSort([10, 30, 3, 6, 1, 23]) == [1, 3, 6, 10, 23, 30]
+    assert countSort([1, 18, 3, 9, 1, 3]) == [1, 1, 3, 3, 9, 18]
+    assert countSort([22, 3, 12, 6, 2, 20]) == [2, 3, 6, 12, 20, 22]
  
  
 
