f59ad57ae0ca83734796ec305ed3185be68ffb8a
Fernando <toshk15@hotmail.com>
2025-07-25 21:04:58 -0600

    fix test cases

--- src/app.py
+++ src/app.py
@@ -74,6 +74,39 @@ def insertSort(arr):
 
     return arr
 
+def mergeSort(arr):
+    if len(arr) > 1:        
+    
+        mid = len(arr)//2
+        left = arr[:mid]
+        right = arr[mid:]
+        mergeSort(left)
+        mergeSort(right)
+        
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i+=1            
+            else:
+                arr[k] = right[j]
+                j+=1
+            k+=1
+
+        while i < len(left):
+            arr[k] = left[i]
+            i+=1
+            k+=1
+
+        while j < len(right):
+            arr[k] = right[j]  
+            j+=1
+            k+=1
+
+    return arr    
+
+
 if __name__ == "__main__":
     pass
     
