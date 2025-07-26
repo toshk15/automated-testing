2b2a920bd8463c9f4b94a6b34c96247aad99dd0b
Fernando <toshk15@hotmail.com>
2025-07-25 21:42:57 -0600

    add quick sort

--- src/app.py
+++ src/app.py
@@ -106,6 +106,17 @@ def mergeSort(arr):
 
     return arr    
 
+def quickSort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    pivot = arr[len(arr)//2]
+    left = [x for x in arr if x < pivot]
+    mid = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+
+    return quickSort(left) + mid + quickSort(right)
+
 
 if __name__ == "__main__":
     pass
--- test/test_app.py
+++ test/test_app.py
@@ -1,4 +1,4 @@
-from src.app import selectionSort, shellSort, countSort, insertSort, bubbleSort, mergeSort
+from src.app import selectionSort, shellSort, countSort, insertSort, bubbleSort, mergeSort, quickSort
 
 def test_selectionSort_function():
     assert selectionSort([10, 30, 3, 6, 1, 23]) == [1, 3, 6, 10, 23, 30]
@@ -29,3 +29,8 @@ def test_mergeSort_function():
     assert mergeSort([10, 30, 3, 6, 1, 23]) == [1, 3, 6, 10, 23, 30]
     assert mergeSort([1, 18, 3, 9, 1, 3]) == [1, 1, 3, 3, 9, 18]
     assert mergeSort([22, 3, 12, 6, 2, 20]) == [2, 3, 6, 12, 20, 22]
+
+def test_quickeSort_function():
+    assert quickSort([10, 30, 3, 6, 1, 23]) == [1, 3, 6, 10, 23, 30]
+    assert quickSort([1, 18, 3, 9, 1, 3]) == [1, 1, 3, 3, 9, 18]
+    assert quickSort([22, 3, 12, 6, 2, 20]) == [2, 3, 6, 12, 20, 22]
